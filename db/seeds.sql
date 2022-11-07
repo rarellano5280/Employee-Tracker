8eaab0c307b7c9907cfa269082571940d14b7537
Robert <109295306+rarellano5280@users.noreply.github.com>
2022-11-07 15:07:30 -0700

    got seeds.sql to work without errors

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,15 +1,28 @@
 INSERT INTO department (name)
-VALUES ("Human Resources"),
-        ("Sales"),
-        ("Engineering"),
-        ("Payroll")
-        ("Marketing");
+VALUES ("human Resources"),
+        ("sales"),
+        ("engineering"),
+        ("payroll"),
+        ("marketing");
+
+SELECT * FROM department;
     
 INSERT INTO role (title, salary, department_id)
 VALUES ("HR Director", 100000, 1),
     ("Sales Rep", 75000, 2),
-    ("Sales Manager", 95000, 2)
+    ("Sales Manager", 95000, 2),
     ("Software Developer", 110000, 3),
-    ("Junior Developer", 80000, 3)
+    ("Junior Developer", 80000, 3),
     ("Payroll Specialist", 85000, 4),
-    ("Marketing Intern" 60000, 5)
+    ("Marketing Intern", 60000, 5);
+
+SELECT * FROM role;
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("Thomas", "White", 1, 1),
+    ("Chase", "Giffin", 3, 2),
+    ("Jessie", "Smith", 2, 3),
+    ("Forest", "Grahm", 4, 4),
+    ("Robert", "Arellano", 3, 2);
+
+SELECT * FROM employee;
