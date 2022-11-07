2af8f57018d0d7ab6fe19b310dba376df9f97740
Robert <109295306+rarellano5280@users.noreply.github.com>
2022-11-06 21:11:50 -0700

    Finished schema.sql, working on seeds.sql file

---
+++ db/seeds.sql
@@ -0,0 +1,15 @@
+INSERT INTO department (name)
+VALUES ("Human Resources"),
+        ("Sales"),
+        ("Engineering"),
+        ("Payroll")
+        ("Marketing");
+    
+INSERT INTO role (title, salary, department_id)
+VALUES ("HR Director", 100000, 1),
+    ("Sales Rep", 75000, 2),
+    ("Sales Manager", 95000, 2)
+    ("Software Developer", 110000, 3),
+    ("Junior Developer", 80000, 3)
+    ("Payroll Specialist", 85000, 4),
+    ("Marketing Intern" 60000, 5)
